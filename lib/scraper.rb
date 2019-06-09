4bc945c5e0ead42c2ca2c72d0a932685b0410770
lanibon <nguyenlan13@gmail.com>
2019-06-09 14:50:12 -0700

    defined scrape_name method

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,6 +1,18 @@
 class Scraper
 
-    def self.scrape_name
+    def get_page
+        Nokogiri::HTML(open("https://www.lafitness.com/Pages/AerobicClasses.aspx"))
+      end
 
+    def get_name_listing
+        self.get_page.css(".classMoreInfo")
+    end
+
+    def self.scrape_name
+        self.get_name_listing.each do |info|
+            group_fitness = GroupFitness.new
+            group_fitness.name = info.css(".lah3").text
+            group_fitness.description = info.css("#ctl00_MainContent_ClassContentGroup1_rptClasses_ctl01_lblClassFullDesc").text        end
+        end
     end
 end
