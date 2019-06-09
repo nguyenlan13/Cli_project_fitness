c40c2e4af272b477d130deb2de64097984abd775
lanibon <nguyenlan13@gmail.com>
2019-06-08 17:45:42 -0700

    Initial commit

---
+++ lib/environment.rb
@@ -0,0 +1,8 @@
+require "fitness/version"
+
+module Fitness
+  class Error < StandardError; end
+  # Your code goes here...
+end
+
+require_relative './fitness_classes'
---
+++ lib/fitness/version.rb
@@ -0,0 +1,3 @@
+module Fitness
+  VERSION = "0.1.0"
+end
