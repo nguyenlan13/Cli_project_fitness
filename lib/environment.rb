077aff9c304a2caa98849f6453c3ee7673811422
lanibon <nguyenlan13@gmail.com>
2019-06-09 13:17:12 -0700

    completed environment

--- lib/environment.rb
+++ lib/environment.rb
@@ -1,8 +1,15 @@
-require "fitness/version"
+require 'pry'
+require 'nokogiri'
+require 'open-uri'
+
+
 
 module Fitness
   class Error < StandardError; end
   # Your code goes here...
 end
 
+require_relative "./fitness/version"
 require_relative './fitness_classes'
+require_relative './cli'
+require_relative './scraper'
