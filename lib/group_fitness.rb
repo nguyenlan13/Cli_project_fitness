2959a3a56a1560f6a23ce99de61d8e938e7358e6
lanibon <nguyenlan13@gmail.com>
2019-06-23 13:15:22 -0700

    change in GroupFitness reader

--- lib/group_fitness.rb
+++ lib/group_fitness.rb
@@ -2,11 +2,11 @@ class GroupFitness
 
     @@all = []
 
-    attr_accessor :description, :gym_location, :fitness_class_id
+    attr_accessor :description, :gym_location 
     attr_writer :zip_code
-    attr_reader :name
+    attr_reader :name, :fitness_class_id
   
-    def initialize(name, description=nil, fitness_class_id=nil)
+    def initialize(name, description=nil, fitness_class_id)
         @name = name
         @description = description
         @fitness_class_id = fitness_class_id
